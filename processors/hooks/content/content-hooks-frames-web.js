a807ed2fc359e4f6fcfd03957054ef284ebc1595
Gildas <gildas.lormeau@gmail.com>
2022-11-29 02:40:24 +0100

    set width and height cf https://github.com/gildas-lormeau/SingleFile/issues/1061

--- processors/hooks/content/content-hooks-frames-web.js
+++ processors/hooks/content/content-hooks-frames-web.js
@@ -132,6 +132,10 @@
 					});
 					image.onload = image.onloadend = image.onerror = event => {
 						dispatchEvent(new CustomEvent(IMAGE_LOADED_EVENT, { detail: image.src }));
+						result.__defineGetter__("height", () => image.height);
+						result.__defineGetter__("width", () => image.width);
+						result.__defineGetter__("naturalHeight", () => image.naturalHeight);
+						result.__defineGetter__("naturalWidth", () => image.naturalWidth);
 						result.dispatchEvent(new UIEvent(event.type, event));
 					};
 					if (image.decode) {
