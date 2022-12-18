ad3862349b1db36e29c61d33072e5a2911e469bc
Gildas <gildas.lormeau@gmail.com>
2022-12-18 03:19:29 +0100

    format code

--- processors/lazy/content/content-lazy-loader.js
+++ processors/lazy/content/content-lazy-loader.js
@@ -72,13 +72,12 @@ async function process(options) {
 		const bodyHeight = (document.body && document.body.scrollHeight) || document.documentElement.scrollHeight;
 		const bodyWidth = (document.body && document.body.scrollWidth) || document.documentElement.scrollWidth;
 		if (bodyHeight > globalThis.innerHeight || bodyWidth > globalThis.innerWidth) {
-			const maxScrollY =  Math.max(bodyHeight - (globalThis.innerHeight * 1.5), 0);
-			const maxScrollX =  Math.max(bodyWidth - (globalThis.innerWidth * 1.5), 0);
-			if (globalThis.scrollY < maxScrollY ||  globalThis.scrollX < maxScrollX) {
+			const maxScrollY = Math.max(bodyHeight - (globalThis.innerHeight * 1.5), 0);
+			const maxScrollX = Math.max(bodyWidth - (globalThis.innerWidth * 1.5), 0);
+			if (globalThis.scrollY < maxScrollY || globalThis.scrollX < maxScrollX) {
 				return triggerLazyLoading(options);
 			}
 		}
-		
 	}
 }
 
