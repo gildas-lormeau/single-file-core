940283c66cab5cb4883cd1408ead6055da0df319
Gildas <gildas.lormeau@gmail.com>
2023-06-21 16:22:52 +0200

    fix missing parameter fix https://github.com/gildas-lormeau/SingleFile/issues/1175 see https://github.com/gildas-lormeau/SingleFileZ/issues/154

--- modules/template-formatter.js
+++ modules/template-formatter.js
@@ -1,23 +1,23 @@
 /*
  * Copyright 2010-2022 Gildas Lormeau
  * contact : gildas.lormeau <at> gmail.com
- * 
+ *
  * This file is part of SingleFile.
  *
- *   The code in this file is free software: you can redistribute it and/or 
- *   modify it under the terms of the GNU Affero General Public License 
+ *   The code in this file is free software: you can redistribute it and/or
+ *   modify it under the terms of the GNU Affero General Public License
  *   (GNU AGPL) as published by the Free Software Foundation, either version 3
  *   of the License, or (at your option) any later version.
- * 
- *   The code in this file is distributed in the hope that it will be useful, 
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of 
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero 
+ *
+ *   The code in this file is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero
  *   General Public License for more details.
  *
- *   As additional permission under GNU AGPL version 3 section 7, you may 
- *   distribute UNMODIFIED VERSIONS OF THIS file without the copy of the GNU 
- *   AGPL normally required by section 4, provided you include this license 
- *   notice and a URL through which recipients can access the Corresponding 
+ *   As additional permission under GNU AGPL version 3 section 7, you may
+ *   distribute UNMODIFIED VERSIONS OF THIS file without the copy of the GNU
+ *   AGPL normally required by section 4, provided you include this license
+ *   notice and a URL through which recipients can access the Corresponding
  *   Source.
  */
 
@@ -1908,9 +1908,9 @@ const EMOJIS = Object.keys(EMOJI_NAMES);
 export { formatFilename, evalTemplate };
 
 async function formatFilename(content, options, util) {
-	let filename = (await evalTemplate(options.filenameTemplate, options, content)) || "";
+	let filename = (await evalTemplate(options.filenameTemplate, options, util, content)) || "";
 	if (options.replaceEmojisInFilename) {
-		EMOJIS.forEach(emoji => filename = filename.replaceAll(emoji, " _" + EMOJI_NAMES[emoji] + "_ "));
+		EMOJIS.forEach(emoji => (filename = filename.replaceAll(emoji, " _" + EMOJI_NAMES[emoji] + "_ ")));
 	}
 	const replacementCharacter = options.filenameReplacementCharacter;
 	filename = getValidFilename(filename, options.filenameReplacedCharacters, replacementCharacter);
