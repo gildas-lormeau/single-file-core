0cbb1712e43a8701be8ac69e85c5ae62246c8bc0
Gildas <gildas.lormeau@gmail.com>
2026-09-14 17:24:37 +0200

    default the creation zip bundle to no worker uri

--- zip-build/lib/zip-vendor.js
+++ zip-build/lib/zip-vendor.js
@@ -1,3 +1,9 @@
+import { setDefaultConfiguration } from "../node_modules/@zip.js/zip.js/lib/core/configuration.js";
+
+setDefaultConfiguration({
+	workerURI: null
+});
+
 export * from "../node_modules/@zip.js/zip.js/lib/zip-core.js";
 export { initStream, readUint8Array } from "../node_modules/@zip.js/zip.js/lib/core/io.js";
 export { deflateSync as deflateRaw, inflateSync as inflateRaw } from "./fflate.js";
